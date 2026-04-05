983eb9a614327ca8d104c04b08fff446f76a2dd2
zhouxiang <zhouxiang@pxb7.com>
2026-04-05 20:36:19 +0800

    feat：结构初始化

---
+++ genesis-client/src/main/java/com/vov/client/api/DemoServiceI.java
@@ -0,0 +1,10 @@
+package com.vov.client.api;
+
+/**
+ * IM客服端相关接口
+ * @author chenminhua
+ * @date 2025/11/13
+ */
+public interface DemoServiceI {
+
+}
---
+++ genesis-client/src/main/java/com/vov/client/response/DemoRespDTO.java
@@ -0,0 +1,9 @@
+package com.vov.client.response;
+
+/**
+ * @desc:
+ * @author: ZhouXiang
+ * @Time：2025/11/12
+ */
+public class DemoRespDTO {
+}
