aa15748182bd79ccc01998c742bee766d04fdbda
zhouxiang <zhouxiang@35668388.onaliyun.com>
2026-09-05 17:41:47 +0800

    feat: 添加supabase jwt

--- genesis-client/src/main/java/com/vov/client/request/DemoReqDTO.java
+++
@@ -1,9 +0,0 @@
-package com.vov.client.request;
-
-/**
- * @desc:
- * @author: ZhouXiang
- * @Time：2025/11/12
- */
-public class DemoReqDTO {
-}
--- genesis-client/src/main/java/com/vov/client/response/DemoRespDTO.java
+++
@@ -1,9 +0,0 @@
-package com.vov.client.response;
-
-/**
- * @desc:
- * @author: ZhouXiang
- * @Time：2025/11/12
- */
-public class DemoRespDTO {
-}
